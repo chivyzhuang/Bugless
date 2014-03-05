a4b5492f9b83c2b01ba954ca5b37e85bf8707095
Chivy Zhuang <chivyzhuang@gmail.com>
2014-03-05 22:10:37 +0800

    correct settings err

--- QaPlatform/settings.py
+++ QaPlatform/settings.py
@@ -22,7 +22,6 @@ MANAGERS = ADMINS
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
@@ -43,7 +42,7 @@ DATABASES = {
 	'HOST': '/var/run/mysqld/mysqld.sock'
     }
 }
-
+'''
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 
