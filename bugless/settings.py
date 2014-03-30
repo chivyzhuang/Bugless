36e31cc7bdbb7f91351ae3bb7453c3a22b583c72
Chivy Zhuang <chivyzhuang@gmail.com>
2014-03-30 12:09:32 +0800

    revert settings.py

--- bugless/settings.py
+++ bugless/settings.py
@@ -17,7 +17,6 @@ MANAGERS = ADMINS
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
@@ -38,6 +37,7 @@ DATABASES = {
 	'PORT': '3306'
     }
 }
+'''
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 
