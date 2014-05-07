49f5e04c1812eb74cb6d304ce8fdfd709a062c3a
Chivy Zhuang <chivyzhuang@gmail.com>
2014-05-07 14:46:48 +0800

    修改settings的bug
    1. 数据库信息不直接放在settings内，改由dj_database_url解析
    2. 使用foreman运行的时候，import django的reverse函数会出错，改正

--- bugless/settings.py
+++ bugless/settings.py
@@ -20,32 +20,23 @@ DEFAULT_CHARSET = 'utf-8'
 
 MANAGERS = ADMINS
 
-AUTH_PROFILE_MODULE = 'accounts.UserProfile'
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'd24vtv9eieu8i4',                      # Or path to database file if using sqlite3.
-        'USER': 'gstelqubylctsp',                      # Not used with sqlite3.
-        'PASSWORD': 'Rki2qawyRBVNj6UwFTBlcK3SsF',                  # Not used with sqlite3.
-        'HOST': 'ec2-54-197-241-95.compute-1.amazonaws.com',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
-'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'bugless',
-    	'USER': 'qaer',
-    	'PASSWORD': 'qaer',
-    	'PORT': '3306'
+        'USER': 'qaer',
+        'PASSWORD': 'qaer',
+        'PORT': '3306'
     }
 }
-'''
+''''''
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 
@@ -73,7 +64,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'staticfiles'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
@@ -115,6 +106,7 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'bugless.urls'
 
+
 WSGI_APPLICATION = 'bugless.wsgi.application'
 
 
@@ -145,8 +137,7 @@ INSTALLED_APPS = (
 )
 
 
-from django.core.urlresolvers import reverse
-LOGIN_REDIRECT_URL = reverse('accounts:profile_welcome')
+LOGIN_REDIRECT_URL = "/accounts/profile/"
 
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
