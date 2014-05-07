4cf26406934e9f5cc9341e35f3bb380090f3d165
Chivy Zhuang <chivyzhuang@gmail.com>
2014-05-07 13:04:47 +0800

    db设置

--- bugless/settings.py
+++ bugless/settings.py
@@ -24,7 +24,7 @@ AUTH_PROFILE_MODULE = 'accounts.UserProfile'
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
@@ -45,7 +45,7 @@ DATABASES = {
     	'PORT': '3306'
     }
 }
-
+'''
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 
